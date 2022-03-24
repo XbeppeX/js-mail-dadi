a805250f797d4af115ee9ca08167cdb28588f553
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-24 15:16:42 +0100

    fix

--- Esercizio Dadi/main.js
+++ Esercizio Dadi/main.js
@@ -4,9 +4,9 @@ const sceltagiocatore = prompt ("Vuoi giocare a dadi? Digita ok se vuoi giocare"
 // Condizione if per scegliere numero random
 if (sceltagiocatore=="ok"){
 
-    const sceltaComputer = Math.floor(Math.random() * 7);
+    const sceltaComputer = Math.floor(Math.random() * 6) + 1;
     console.log(`Il tuo numero è: ${sceltaComputer}`);
-    const sceltaComputer2 = Math.floor(Math.random() * 7);
+    const sceltaComputer2 = Math.floor(Math.random() * 6) + 1;
     console.log(`Il numero del Computer è: ${sceltaComputer2}`);
 
     if (sceltaComputer > sceltaComputer2){
