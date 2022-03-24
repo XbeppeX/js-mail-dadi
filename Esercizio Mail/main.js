565ea396ad07f813f06e5cd55600e73853957271
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-24 15:53:03 +0100

    bonus

--- Esercizio Mail/main.js
+++ Esercizio Mail/main.js
@@ -1,12 +1,24 @@
+//creazioni array
 let mail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com","anna@gmail.com"];
 let newmail = prompt("Inserisci la tua email!");
 
-for(let i=0; i < mail.length; i++){
-    
-    if (newmail == mail[i]){
-        console.log("La tua email corrisponde, Benvenuto!");
-        break;
-    }else {
-        console.log("La tua email non corrisponde");
+// if con ".includes" per check in lista
+if (mail.includes(newmail)){
+    console.log("La tua email corrisponde, Benvenuto!");
+}else {
+
+    console.log("La tua email non corrisponde");
+
+    //inserimento email ad array
+    const risposta = prompt("Vuoi inserire la tua email alla lista ?");
+
+    if (risposta == "si") {
+        mail.push(newmail);
+        console.log(mail);
+    } else if (risposta == "no") {
+        console.log("Nessun problema, grazie comunque");
     }
+    
 }
+
+
