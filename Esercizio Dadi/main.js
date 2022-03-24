56cccac42b2e140496f57d22c0ecf0f6075cbbb1
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-24 15:03:30 +0100

    dadi-com

--- Esercizio Dadi/main.js
+++ Esercizio Dadi/main.js
@@ -1,14 +1,17 @@
 const sceltagiocatore = prompt ("Vuoi giocare a dadi? Digita ok se vuoi giocare");
 
-const sceltaComputer = Math.floor(Math.random() * 7);
-const scelta1 = parseInt(sceltaComputer);
-console.log(`Il tuo numero è: ${scelta1}`);
-const sceltaComputer2 = Math.floor(Math.random() * 7);
-const scelta2 = parseInt(sceltaComputer2);
-console.log(`Il tuo numero è: ${scelta2}`);
+if (sceltagiocatore=="ok"){
 
-if (scelta1 > scelta2){
-    console.log("hai vinto")
-}else {
-    console.log("hai perso")
+    const sceltaComputer = Math.floor(Math.random() * 7);
+    const scelta1 = parseInt(sceltaComputer);
+    console.log(`Il tuo numero è: ${scelta1}`);
+    const sceltaComputer2 = Math.floor(Math.random() * 7);
+    const scelta2 = parseInt(sceltaComputer2);
+    console.log(`Il numero del Computer è: ${scelta2}`);
+
+    if (scelta1 > scelta2){
+        console.log("hai vinto");
+    }else {
+        console.log("hai perso");
+    }
 }
